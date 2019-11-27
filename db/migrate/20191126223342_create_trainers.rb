0b3484d8e07a471cc3d760c924af7cbbc554fc0b
dmandelb <devin.mandelbaum@gmail.com>
2019-11-26 20:01:41 -0500

    Filled out migration for trainers table

--- db/migrate/20191126223342_create_trainers.rb
+++ db/migrate/20191126223342_create_trainers.rb
@@ -1,6 +1,11 @@
 class CreateTrainers < ActiveRecord::Migration[6.0]
   def change
     create_table :trainers do |t|
+      t.string :trainer_name, null: false
+      t.string :email, null: false
+      t.boolean :display_email, default: false
+      t.text :contact_info
+      t.string :location
 
       t.timestamps
     end
