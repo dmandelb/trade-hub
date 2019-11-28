ede6f19720646e098434e8b07dea68d63aba1020
dmandelb <devin.mandelbaum@gmail.com>
2019-11-28 12:51:46 -0500

    Create friendships table and set up preliminary associations

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,4 +1,7 @@
 class Trainer < ApplicationRecord
   validates_presence_of :trainer_name, :email
   validates :team, inclusion: {in: %w(Blue Red Yellow)}
+  has_many :friendships
+  has_many :inverse_friendships, foreign_key: :friend_id
+  has)_many :friends, through: :friendships
 end
---
+++ db/migrate/20191128171838_create_friendships.rb
@@ -0,0 +1,12 @@
+class CreateFriendships < ActiveRecord::Migration[6.0]
+  def change
+    create_table :friendships do |t|
+      t.references :trainer, null: false, foreign_key: true
+      t.references :friend, null: false
+      t.boolean :confirmed, null: false, default: false
+      t.integer :level, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
