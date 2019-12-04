2438c4dd3e1db493f7c0c6e5ae056b841fda9032
dmandelb <devin.mandelbaum@gmail.com>
2019-12-04 15:09:53 -0500

    Changed all boolean columns to null:false, default:false to avoid three-state boolean problem

--- db/migrate/20191204193207_create_entries.rb
+++ db/migrate/20191204193207_create_entries.rb
@@ -3,9 +3,9 @@ class CreateEntries < ActiveRecord::Migration[6.0]
     create_table :entries do |t|
       t.references :dex, null: false, foreign_key: true
       t.references :available_mon, null: false, foreign_key: true
-      t.boolean :filled?
-      t.boolean :available?
-      t.integer :priority
+      t.boolean :filled?, null: false, default: false
+      t.boolean :available?, null: false, default: false
+      t.integer :priority, default: 0
 
       t.timestamps
     end
