3831a46b41b4cd535a6e33d9738114a1debde0c5
dmandelb <devin.mandelbaum@gmail.com>
2019-12-04 15:11:57 -0500

    Fixed typo in dexes migration and migrated all tables

--- db/migrate/20191204192805_create_dexes.rb
+++ db/migrate/20191204192805_create_dexes.rb
@@ -1,7 +1,7 @@
 class CreateDexes < ActiveRecord::Migration[6.0]
   def change
     create_table :dexes do |t|
-      t.refernces :trainer
+      t.references :trainer
 
       t.timestamps
     end
