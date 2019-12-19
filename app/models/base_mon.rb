9a55003351307be8ae74ac2301c5c1a5299530b0
dmandelb <devin.mandelbaum@gmail.com>
2019-12-19 18:53:52 -0500

    Add skeleton of model to update base_mon list for release of new mons

--- app/models/base_mon.rb
+++ app/models/base_mon.rb
@@ -1,2 +1,11 @@
 class BaseMon < ApplicationRecord
+
+  
+  def self.update_list
+    response = Unirest.get "https://pokemon-go1.p.rapidapi.com/released_pokemon.json",
+      headers:{
+        "X-RapidAPI-Host" => ENV["POGO_HOST"],
+        "X-RapidAPI-Key" => ENV["POGO_KEY"]
+      }
+  end
 end
