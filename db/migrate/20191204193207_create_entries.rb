c59ecd8b42b8cd552e603a9e66504c98762dce37
dmandelb <devin.mandelbaum@gmail.com>
2020-01-29 15:07:33 -0500

    Remove question marks from boolean table columns because I swear I actually know better than that

--- db/migrate/20191204193207_create_entries.rb
+++ db/migrate/20191204193207_create_entries.rb
@@ -3,8 +3,8 @@ class CreateEntries < ActiveRecord::Migration[6.0]
     create_table :entries do |t|
       t.references :dex, null: false, foreign_key: true
       t.references :available_mon, null: false, foreign_key: true
-      t.boolean :filled?, null: false, default: false
-      t.boolean :available?, null: false, default: false
+      t.boolean :filled, null: false, default: false
+      t.boolean :available, null: false, default: false
       t.integer :priority, default: 0
 
       t.timestamps
